c43c16d1f210158785df023653e9eba342f70b09
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-03-28 13:31:13 +0200

    creations

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('user-selected', (e) => console.log(e.detail));
+
+const users = [
+    { name: 'sdsadasds', mail: 'sdospds@sdsds.sd' },
+    { name: 'dsds', mail: 'fgfhbnbn@yjtjyj.vd' },
+    { name: 'vcbvbc', mail: 'ereree@aweae.ed' },
+    { name: 'dererr', mail: 'nbvnbbvn@jhkhjkhj.pp' }
+];
+
+for (const user of users) {
+    const pippoTag = document.createElement('pippo-tag');
+    pippoTag.setAttribute('pippo-user', JSON.stringify(user));
+    pippoTag.setAttribute('has-button', '');
+    document.body.appendChild(pippoTag);
+}
+
+function replaceSelected(user) {
+    const selectedContainer = document.getElementById('selected-user');
+    selectedContainer.innerHTML = '';
+    const pippoTag = document.createElement('pippo-tag');
+    pippoTag.setAttribute('pippo-user', JSON.stringify(user));
+    document.selectedContainer.appendChild(pippoTag);
+}
